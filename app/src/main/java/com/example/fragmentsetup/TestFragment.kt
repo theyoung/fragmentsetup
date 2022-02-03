63ade4463beda013c96a7a85f7b48ce757f4ebc5
steven <theyoung2002@naver.com>
2022-02-03 14:21:14 +0900

    view binding

--- app/src/main/java/com/example/fragmentsetup/TestFragment.kt
+++ app/src/main/java/com/example/fragmentsetup/TestFragment.kt
@@ -1,6 +1,8 @@
 package com.example.fragmentsetup
 
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.*
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.navigation.findNavController
+import com.example.fragmentsetup.databinding.FragmentTestBinding
+import java.util.logging.Level.INFO
+import java.util.logging.Logger
 
 
 /**
@@ -16,19 +21,29 @@ import androidx.navigation.findNavController
  * create an instance of this fragment.
  */
 class TestFragment : Fragment() {
+    var _binding: FragmentTestBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_test, container, false)
-        view.findViewById<Button>(R.id.button).setOnClickListener {
-            val str = view.findViewById<EditText>(R.id.editTextTextPersonName).text.toString() ?: "hello"
+
+        _binding = FragmentTestBinding.inflate(inflater,container,false)
+        binding.button.setOnClickListener {
+            val str = binding.editTextTextPersonName.text.toString() ?: "hello"
             val action = TestFragmentDirections.actionTestFragmentToSecondFragment(str)
-            view.findNavController().navigate(action)
+            binding.root.findNavController().navigate(action)
         }
-        return view
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(this.javaClass.name,"finish view")
+        _binding = null
     }
 
 }
